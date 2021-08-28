f0180965b9128e3d4fae86c564da2e2ad44a2e12
jacob l <jacobinmt47@outlook.com>
2021-08-28 12:46:59 -0600

    moves each digit to array and print out

--- src/main/java/sol/Sol1.java
+++ src/main/java/sol/Sol1.java
@@ -10,25 +10,43 @@ package sol;
  * @author jacob
  */
 public class Sol1 {
+
     /**
      * convert int to english words
+     *
      * @param n
      * @return String The english phrase that says the number
      */
-    String intToWord(int n){
+    String intToWord(int n) {
+        int arr[] = new int[10];
         int total = 1;
-        int old  = 0;
+        int d = 1;
+        int old = 1;
         Boolean smaller = true;
-        for (int i = 0; (i < 9)&&smaller; i++) {
-            System.out.println(n%total);
-            smaller = !(n%total == n);
-            System.out.println((n%total - old));
-            old = n%total;
-            total = total*10;
+        for (int i = 0; (i < 9) && smaller; i++) {
+           // System.out.println(n % total);
+            smaller = !(n % total == n);
+            if (total > 10) {
+                d = ((n % total - old) / (total / 10));
+            } 
+            else{
+                d = n%total;
+            }
+            //System.out.println(d);
+            arr[i]=d;
+            old = n % total;
+            total = total * 10;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int j = arr[i];
+            System.out.println(j);
+            
         }
         return "";
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Sol1 s = new Sol1();
         s.intToWord(123456);
     }
